517d1013c68a2b65df2d14dc6c9f7e3992a52be3
Eduarda <duda.eduarda.ec@gmail.com>
2023-03-22 19:48:09 -0300

    Implementação de Postagens e comentários

--- blog/src/app/components/posts/post.component.ts
+++ blog/src/app/components/posts/post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsModel } from 'src/app/shared/model/comments.model';
 import { PostModel } from 'src/app/shared/model/post.model';
 import { PostsService } from 'src/app/shared/services/posts.service';
 
@@ -12,11 +13,19 @@ export class PostComponent implements OnInit{
   constructor(private postService: PostsService){}
   
   allPosts:PostModel[] = []
+  commentsId: CommentsModel[]=[]
 
   ngOnInit(){
     this.postService.getAllPosts().subscribe(
       response=>{
         this.allPosts = response
     })
+  }
+  commentsById(id:number){
+    this.postService.getCommentsId(id).subscribe(
+      response=>{
+        this.commentsId = response
+      }
+    )
   }
 }
--- blog/src/app/shared/services/posts.service.ts
+++ blog/src/app/shared/services/posts.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PostModel } from '../model/post.model';
+import { CommentsModel } from '../model/comments.model'
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,10 @@ export class PostsService {
 
   getAllPosts(): Observable<PostModel[]>{
     return this.http.get<PostModel[]>(`${this.baseUrl}posts`)
+  }
+  
+  getCommentsId(id:number): Observable<CommentsModel[]>{
+    return this.http.get<CommentsModel[]>(`${this.baseUrl}posts/${id}/comments`)
   }
 
 }
